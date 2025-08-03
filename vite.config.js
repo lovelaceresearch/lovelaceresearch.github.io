9d2cb3968140852a9900cbb0a299dfca3630e4d7
Chanwoo Lee <chanwoo.lee@imperial.ac.uk>
2025-08-03 17:32:52 +0100

    Fix GitHub Pages deployment for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === 'serve' ? '/' : '/lovelaceresearch.github.io/'
-  return {
-    plugins: [react()],
-    base: base,
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
 })
