e1184e1a58dd395e8c9a90da879ae3ecb8661755
Chanwoo Lee <chanwoo.lee@imperial.ac.uk>
2025-08-27 21:36:56 +0100

    update

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,8 +1,10 @@
 import Sidebar from "./(components)/Sidebar";
+import ImageBackground from "./(components)/ImageBackground";
 
 export default function Home() {
   return (
     <div className="page-container">
+      <ImageBackground />
       <Sidebar />
       <main className="main-content home-hero">
         <div className="hero-center">
